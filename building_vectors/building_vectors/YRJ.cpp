11ae3027335a9eb78205fb054685245ae4581fab
Younies Mahmoud <youniesmahmoud@youniess-mbp.dhcp.wustl.edu>
2016-07-15 12:07:28 -0500

    yjr

---
+++ building_vectors/building_vectors/YRJ.cpp
@@ -0,0 +1,84 @@
+//
+//  YRJ.cpp
+//  building_vectors
+//
+//  Created by Younies Mahmoud on 7/15/16.
+//  Copyright © 2016 Younies Mahmoud. All rights reserved.
+//
+
+#include "YRJ.hpp"
+
+
+YRJ::YRJ(string path_to_file)
+{
+    this->path_to_file = path_to_file;
+    ifstream fileStream(path_to_file);
+    fileStream.read( (char *)&this->kmerLength  , sizeof(LONG));
+    fileStream.read( (char *) &this->numOfKmers , sizeof(LONG));
+    this->kmersVector = *new vector<LONG>(this->numOfKmers , 0);
+    
+    for (int i = 0 ; i < numOfKmers; ++i)
+    {
+        fileStream.read( (char *) &kmersVector[i], sizeof(LONG));
+    }
+    
+    fileStream.close();
+}
+
+
+
+
+bool YRJ::isKmerExist(LONG kmer)
+{
+    return binary_search(kmersVector.begin(), kmersVector.end(), kmer);
+}
+
+
+
+
+
+vector<LONG> YRJ::getRandomSamples(string file_path , LONG numOfSamples)
+{
+    
+    vector<LONG> samples(numOfSamples);
+    ifstream fileStream(file_path);
+    string line;
+    LONG indx;
+    LONG pointer = 0;
+    while (getline(fileStream, line))
+    {
+        stringstream ss(line);
+        ss >> indx;
+        samples[pointer++] = this->kmersVector[indx];
+    }
+    fileStream.close();
+    
+    return samples;
+}
+
+
+
+
+
+/*
+
+LONG YRJ::getRandomSamples(vector<LONG> &samples , LONG numOfSamples)
+{
+    if (numOfSamples > this->numOfKmers)
+    {
+        numOfSamples = this->numOfKmers;
+        samples = this->kmersVector;
+        return numOfSamples;
+    }
+    
+    
+    
+    
+    
+}
+*/
+
+
+
+
+
---
+++ building_vectors/building_vectors/main.cpp
@@ -0,0 +1,102 @@
+//
+//  main.cpp
+//  building_vectors
+//
+//  Created by Younies Mahmoud on 7/15/16.
+//  Copyright © 2016 Younies Mahmoud. All rights reserved.
+//
+
+#include "headers.h"
+#include "YRJ.hpp"
+
+
+int main(int argc, const char * argv[])
+{
+   /* LONG numberOfSamples , kmerLength;
+    string path_to_database , name_of_database = "kmerDatabase";
+    
+    string path_to_ten_unique_rand = "/export/project/hondius/yr_db/for_garbages/tenMillionRandom.txt";
+    
+    if(argc < 4)
+    {
+        cout << "Usage:: enter the number of sampling , the kmer size and the path to the database \n";
+        return 1;
+    }
+    else
+    {
+        stringstream sampling(argv[1]);
+        sampling >> numberOfSamples;
+        
+        stringstream kmerLS(argv[2]);
+        kmerLS >> kmerLength ;
+        
+        path_to_database = argv[3];
+    }
+    
+    */
+    
+    string path_to_ten_unique_rand = "/export/project/hondius/yr_db/for_garbages/tenMillionRandom.txt";
+    
+    string path_to_container = "/export/project/hondius/newProject/kmerDatabase31/";
+    string path_to_all = "/export/project/hondius/newProject/kmerDatabase31/all.yrj";
+    
+    //for reading all the names
+    LONG numOfUID = 8065;
+    string path_to_names = "/export/project/hondius/newProject/namesOfFiles.txt";
+    vector<string> namesOfFiles;
+    ifstream namesStream(path_to_names);
+    string name;
+    while (getline(namesStream, name))
+    {
+        namesOfFiles.push_back(name);
+    }
+    namesStream.close();
+    //end reading all the names
+
+    YRJ all = *new YRJ(path_to_all);
+    vector<LONG> samples = all.getRandomSamples(path_to_ten_unique_rand, 10000000);
+
+    delete &all;
+    //read all the uids
+    vector<YRJ*> all_UIDs(numOfUID);
+    for (int i = 0 ; i < numOfUID; ++i)
+    {
+        all_UIDs[i] = new YRJ(namesOfFiles[i]);
+    }
+    
+    string path_to_result = "/export/project/hondius/newProject/result_ten_million.txt";
+    ofstream os(path_to_result);
+    
+    
+    
+    for (int i = 0 , n = (int)samples.size(); i < n ; ++i)
+    {
+        char * c;
+        int count1 = 0 , count0 = 0;
+        for (int j = 0  ; j < numOfUID; ++j)
+        {
+            
+            if(all_UIDs[j]->isKmerExist(samples[i]))
+            {
+                ++count1;
+                *c = '1';
+            }
+            else
+            {
+                ++count0;
+                *c = '0';
+            }
+            os.write(c, sizeof(char));
+        }
+        *c = '\n';
+        os.write(c, sizeof(char));
+        cerr << "cout0 : " << count0 << "   count1:  " << count1 << endl;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
